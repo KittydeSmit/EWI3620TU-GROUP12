5e4cc3a04b485420af8254760f55a1c968999d25
thomask93 <T.P.S.Kotte@student.tudelft.nl>
2013-11-14 09:21:42 +0100

    Comment toegevoegd

--- Buttons/src/Button.java
+++ Buttons/src/Button.java
@@ -11,24 +11,24 @@ public class Button {
 	public Button(){
 	}
 	
-	public static JButton newButton(String filename1, String filename2, String filename3){
+	public static JButton newButton(String buttonfile, String buttonrolloverfile, String buttonpressedfile){
 		JButton button = new JButton();
-		
+		//Geef de button zelfde grootte als het plaatje
 		Dimension d=new Dimension(151,89);
 		button.setMaximumSize(d);
-		
-		Image plaatje=Toolkit.getDefaultToolkit().getImage(filename1);
+		//Maakt van de button een plaatje
+		Image plaatje=Toolkit.getDefaultToolkit().getImage(buttonfile);
 		Icon icoon=new ImageIcon(plaatje);
 		button.setIcon(icoon);
-		
-		Image a=Toolkit.getDefaultToolkit().getImage(filename2);
+		//Maakt van de button een ander plaatje wanneer de muis eroverheen gaat
+		Image a=Toolkit.getDefaultToolkit().getImage(buttonrolloverfile);
 		Icon icoon2=new ImageIcon(a);
 		button.setRolloverIcon(icoon2);
-		
-		Image b=Toolkit.getDefaultToolkit().getImage(filename3);
+		//Maak van de button een ander plaatje wanneer deze ingedrukt wordt
+		Image b=Toolkit.getDefaultToolkit().getImage(buttonpressedfile);
 		Icon icoon3=new ImageIcon(b);
 		button.setPressedIcon(icoon3);
-		
+		//Zorgt ervoor dat van de button alleen het plaatje te zien is
 		button.setContentAreaFilled(false);
 		button.setBorderPainted(false);
 		button.setFocusable(false);
